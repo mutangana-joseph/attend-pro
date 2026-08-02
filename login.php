47dfd5970518a560736222d0b15550184baea75b
mutanganajoseph <josephmutangana65@gmail.com>
2026-08-02 09:31:54 +0200

    Completed login backend

--- login.php
+++ login.php
@@ -3,6 +3,48 @@
 $pageTitle = "Login";
 
 require "includes/header.php";
+require "config/db.php";
+require "includes/functions.php";
+$error = "";
+
+
+
+if($_SERVER["REQUEST_METHOD"] === "POST"){
+    $email = trim($_POST["email"]);
+    $password = trim($_POST["password"]);
+
+    if(empty($email) || empty($password)){
+        $error = "please enter your email and password";
+    }
+    else{
+        
+        $sql = "select * from users where email = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        
+        if($result->num_rows > 0){
+            $user = $result->fetch_assoc();
+            $hashed_password = $user["password"];
+
+            if(!password_verify($password, $hashed_password)){
+                $error = "Invalid password ";
+                
+            }
+            else{
+                $_SESSION["id"] = $user["id"];
+                $_SESSION["name"] = $user["first_name"];
+                header("Location: dashboard.php");
+                exit();
+            }
+        }
+        else{
+            $error = "Account not found";
+        }
+       
+    }
+}
 
 ?>
 
@@ -22,6 +64,22 @@ require "includes/header.php";
 
         </div>
 
+        <?php if(!empty($error)): ?>
+            <div class="error-box">
+            <i class="fa-solid fa-circle-xmark"></i>
+
+            <div>
+
+                <h3>Login Failed</h3>
+
+                <p><?= htmlspecialchars($error) ?></p>
+
+            </div>
+
+
+            </div>
+            <?php endif; ?>
+
         <form action="login.php" method="POST">
 
             <div class="form-group">
@@ -32,6 +90,7 @@ require "includes/header.php";
                 type="email"
                 name="email"
                 placeholder="Enter your email"
+                value="<?= htmlspecialchars($_POST["email"] ?? '')?>"
                 required>
 
             </div>
