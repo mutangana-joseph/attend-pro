5d3e15e2c3ccf1bfb34af4e6c72dce2be4e65f1e
mutanganajoseph <josephmutangana65@gmail.com>
2026-08-02 18:08:03 +0200

    Added statistics in dashboard

--- dashboard.php
+++ dashboard.php
@@ -9,6 +9,36 @@ $pageTitle = "Dashboard";
 $page = "dashboard";
 require "includes/header.php";
 require "includes/sidebar.php";
+require "config/db.php";
+
+
+$is_recorded = false;
+
+
+$current_date = date("y-m-d");
+
+
+$sql = "select attendance_date from attendance where attendance_date = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $current_date);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if($result->num_rows > 0){
+    $is_recorded = true;
+    
+}
+
+$sql = "SELECT DATE_FORMAT(created_at, '%Y-%m-%d %h:%i %p') AS recorded_datetime
+        FROM attendance
+        ORDER BY attendance_date DESC
+        LIMIT 1";
+
+$result = $conn->query($sql);
+
+$row = $result->fetch_assoc();
+
+$last_attendance = $row['recorded_datetime'];
 
 
 
@@ -38,7 +68,7 @@ require "includes/sidebar.php";
 
 
     <!-- Statistics -->
-
+     <?php require "includes/statistics.php";?>
     <section class="stats">
 
         <div class="card stat-card">
@@ -47,7 +77,7 @@ require "includes/sidebar.php";
 
                 <h3>Total Students</h3>
 
-                <h2>245</h2>
+                <h2><?= $total ?></h2>
 
             </div>
 
@@ -61,7 +91,7 @@ require "includes/sidebar.php";
 
                 <h3>Present Today</h3>
 
-                <h2>230</h2>
+                <h2><?= $present ?></h2>
 
             </div>
 
@@ -75,7 +105,7 @@ require "includes/sidebar.php";
 
                 <h3>Absent Today</h3>
 
-                <h2>10</h2>
+                <h2><?= $absent ?></h2>
 
             </div>
 
@@ -89,7 +119,7 @@ require "includes/sidebar.php";
 
                 <h3>Late Today</h3>
 
-                <h2>5</h2>
+                <h2><?= $late ?></h2>
 
             </div>
 
@@ -102,13 +132,57 @@ require "includes/sidebar.php";
 
     <!-- Attendance Status -->
 
+    <?php if($is_recorded): ?>
     <section class="card attendance-status">
 
         <h2>Today's Attendance</h2>
 
         <p>
 
-            Today's attendance has not been recorded yet.
+            Today's attendance has been recorded .
+
+        </p>
+
+        <a href="attendance.php" class="btn">
+
+            <i class="fa-solid fa-calendar-check"></i>
+
+            Update Attendance
+
+        </a>
+
+    </section>
+    <section class="card recent-activity">
+
+        <h2>
+
+            <i class="fa-solid fa-clock-rotate-left"></i>
+
+            Recent Activity
+
+        </h2>
+
+        <ul>
+
+            <li>
+
+                <?= $last_attendance?> Attendance recorded 
+
+            </li>
+
+        </ul>
+
+    </section>
+
+    <?php endif; ?>
+    <?php if(!$is_recorded): ?>
+    <section class="card attendance-status">
+
+        <h2>Today's Attendance</h2>
+
+        <p>
+
+            Today's attendance has not been recorded .
 
         </p>
 
@@ -121,8 +195,6 @@ require "includes/sidebar.php";
         </a>
 
     </section>
-
-
     <!-- Recent Activity -->
 
     <section class="card recent-activity">
@@ -146,6 +218,11 @@ require "includes/sidebar.php";
         </ul>
 
     </section>
+
+    
+    <?php endif; ?>
+
+    
 
 </main>
 
